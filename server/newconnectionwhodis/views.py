f98c8302f8037e3333e8ee1f48277249e7c67723
Exanut <nihilch@protonmail.com>
2021-10-02 13:08:41 -0600

    authors/ correct format

--- server/newconnectionwhodis/views.py
+++ server/newconnectionwhodis/views.py
@@ -1,8 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .serializers import AuthorSerializer
 from .models import Author
 
+class ListAuthors(APIView):
+    def get(self, request):
+        response = {}
+        response["type"] = "authors"
+        response["items"] = AuthorSerializer(
+            Author.objects.all(), context={'request': request}, many=True).data
+        return Response(response)
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all().order_by('displayName')

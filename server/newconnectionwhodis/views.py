404833b2f3402da328a1a1694020849754f85098
Dylan DeCoursey <decourse@ualberta.ca>
2021-10-03 18:03:55 -0600

    Paginating comments

--- server/newconnectionwhodis/views.py
+++ server/newconnectionwhodis/views.py
@@ -1,6 +1,8 @@
 from django.db.models import query
+from django.http import response
 from rest_framework import viewsets
 from rest_framework.response import Response
+from django.core.paginator import Paginator
 
 from . import serializers
 from . import models
@@ -36,9 +38,24 @@ class PostViewSet(viewsets.ModelViewSet):
         return models.Post.objects.filter(author=self.kwargs['author_pk'])
 
 class CommentViewSet(viewsets.ModelViewSet):
-    # TODO: Pagination
     serializer_class = serializers.CommentSerializer
+
     def get_queryset(self):
-        return models.Comment.objects.filter(
+        author_post_comments = models.Comment.objects.order_by('-published').filter(
             author=self.kwargs['author_pk'],
             post=self.kwargs['posts_pk'])
+        num_comments = len(author_post_comments)
+        if num_comments <= 1:
+            return author_post_comments
+        page = self.request.query_params.get('page')
+        size = self.request.query_params.get('size')
+        if page:
+            page = int(page)
+        if size:
+            size = int(size)
+        if not page:
+            page = 1
+        page -= 1
+        if not size:
+            size = num_comments
+        return author_post_comments[page*size : page*size+size]

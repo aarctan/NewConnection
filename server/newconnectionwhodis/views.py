3151e782a5b7bbb5fdf8661780994a74812ed5b2
Exanut <nihilch@protonmail.com>
2021-10-03 18:07:22 -0600

    refactor: DRY views.py

--- server/newconnectionwhodis/views.py
+++ server/newconnectionwhodis/views.py
@@ -1,32 +1,34 @@
-from django.db.models import query
-from django.http import response
 from rest_framework import viewsets
 from rest_framework.response import Response
-from django.core.paginator import Paginator
 
 from . import serializers
 from . import models
 
+def paginate_queryset(query_params, queryset):
+    """
+    Utility function to slice a queryset if pagination parameters
+    'page' and 'size' are provided.
+    """
+    num_models = len(queryset)
+    # slicing empty or singleton querysets does not return queryset.
+    if num_models <= 1:
+        return queryset
+    page = query_params.get('page')
+    size = query_params.get('size')
+    page = int(page) - 1 if page else 0
+    size = int(size) if size else num_models
+    return queryset[page * size : page * size + size]
+
 class AuthorsViewSet(viewsets.ViewSet):
     def list(self, request):
-        page = self.request.query_params.get('page')
-        size = self.request.query_params.get('size')
-        if page:
-            page = int(page)
-        if size:
-            size = int(size)
-        num_authors = len(models.Author.objects.all())
-        if not page:
-            page = 1
-        page -= 1
-        if not size:
-            size = num_authors
-        response = {}
-        response["type"] = "authors"
-        response["items"] = serializers.AuthorSerializer(
-            models.Author.objects.all()[page*size : page*size+size],
-            context={'request': request}, many=True).data
-        return Response(response)
+        return Response({
+            'type': "authors",
+            'items': serializers.AuthorSerializer(
+                paginate_queryset(
+                    self.request.query_params,
+                    models.Author.objects.all()),
+                context={'request': request},
+                many=True).data})
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = models.Author.objects.all().order_by('displayName')
@@ -39,23 +41,9 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CommentSerializer
-
     def get_queryset(self):
-        author_post_comments = models.Comment.objects.order_by('-published').filter(
-            author=self.kwargs['author_pk'],
-            post=self.kwargs['posts_pk'])
-        num_comments = len(author_post_comments)
-        if num_comments <= 1:
-            return author_post_comments
-        page = self.request.query_params.get('page')
-        size = self.request.query_params.get('size')
-        if page:
-            page = int(page)
-        if size:
-            size = int(size)
-        if not page:
-            page = 1
-        page -= 1
-        if not size:
-            size = num_comments
-        return author_post_comments[page*size : page*size+size]
+        return paginate_queryset(
+            self.request.query_params,
+            models.Comment.objects.order_by('-published').filter(
+                author=self.kwargs['author_pk'],
+                post=self.kwargs['posts_pk']))

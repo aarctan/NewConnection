07dd227da5397a47a894fcd6deaeffa261d47feb
Exanut <nihilch@protonmail.com>
2021-10-07 16:17:53 -0600

    revised registration page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,15 +6,14 @@ import {
   Box,
   Button,
   Container,
-  FormHelperText,
   Link,
   TextField,
   Typography,
 } from "@mui/material";
+import GitHub from '@mui/icons-material/GitHub';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const Register = () => {
-  const navigate = useNavigate();
-
   return (
     <>
       <Helmet>
@@ -31,160 +30,78 @@ const Register = () => {
         <Container maxWidth="sm">
           <Formik
             initialValues={{
-              email: "",
-              firstName: "",
-              lastName: "",
+              username: "",
+              displayName: "",
               password: "",
               policy: false,
             }}
             validationSchema={Yup.object().shape({
-              email: Yup.string()
-                .email("Must be a valid email")
+              displayName: Yup.string()
                 .max(255)
-                .required("Email is required"),
-              firstName: Yup.string()
-                .max(255)
-                .required("First name is required"),
-              lastName: Yup.string().max(255).required("Last name is required"),
+                .required("Display name is required"),
               password: Yup.string().max(255).required("password is required"),
             })}
-            onSubmit={(values) => {
-              const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_AUTH_KEY}`;
-              fetch(url, {
-                method: "POST",
-                body: JSON.stringify({
-                  email: values.email,
-                  password: values.password,
-                  returnSecureToken: true,
-                }),
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              })
-                .then((res) => {
-                  //setIsLoading(false);
-                  if (res.ok) {
-                    return res.json();
-                  } else {
-                    return res.json().then((data) => {
-                      let errorMessage = "Authentication failed!";
-                      // if (data && data.error && data.error.message) {
-                      //   errorMessage = data.error.message;
-                      // }
-
-                      throw new Error(errorMessage);
-                    });
-                  }
-                })
-                .then((data) => {
-                  /*
-                    const expirationTime = new Date(
-                      new Date().getTime() + +data.expiresIn * 1000
-                    );
-                    */
-                  //authCtx.login(data.idToken, expirationTime.toISOString());
-                  navigate("/app/dashboard", { replace: true });
-                })
-                .catch((err) => {
-                  alert(err.message);
-                });
-            }}
           >
-            {({
-              errors,
-              handleBlur,
-              handleChange,
-              handleSubmit,
-              isSubmitting,
-              touched,
-              values,
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <Box sx={{ mb: 0 }}>
-                  <Typography color="textPrimary" variant="h2">
-                    Create new account
-                  </Typography>
-                  <Typography
-                    color="textSecondary"
-                    gutterBottom
-                    variant="body1"
-                  >
-                    Use your email to create new account
-                  </Typography>
-                </Box>
-                <TextField
-                  error={Boolean(touched.firstName && errors.firstName)}
-                  fullWidth
-                  helperText={touched.firstName && errors.firstName}
-                  label="First name"
-                  margin="normal"
-                  name="firstName"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.firstName}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.lastName && errors.lastName)}
-                  fullWidth
-                  helperText={touched.lastName && errors.lastName}
-                  label="Last name"
-                  margin="normal"
-                  name="lastName"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.lastName}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.email && errors.email)}
-                  fullWidth
-                  helperText={touched.email && errors.email}
-                  label="Email Address"
-                  margin="normal"
-                  name="email"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="email"
-                  value={values.email}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.password && errors.password)}
-                  fullWidth
-                  helperText={touched.password && errors.password}
-                  label="Password"
-                  margin="normal"
-                  name="password"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="password"
-                  value={values.password}
-                  variant="outlined"
-                />
-                {Boolean(touched.policy && errors.policy) && (
-                  <FormHelperText error>{errors.policy}</FormHelperText>
-                )}
-                <Box sx={{ py: 2 }}>
-                  <Button
-                    color="primary"
-                    disabled={isSubmitting}
-                    fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                  >
-                    Sign up now
-                  </Button>
-                </Box>
-                <Typography color="textSecondary" variant="body1">
-                  Have an account?{" "}
-                  <Link component={RouterLink} to="/login" variant="h6">
-                    Sign in
-                  </Link>
+            <form>
+              <Box sx={{ mb: 0 }}>
+                <Typography color="textPrimary" variant="h2" align="center">
+                  Create an account
                 </Typography>
-              </form>
-            )}
+              </Box>
+              <TextField
+                id="username"
+                fullWidth
+                label="Username"
+                margin="dense"
+                variant="outlined"
+              />
+              <TextField
+                id="displayName"
+                fullWidth
+                label="Display Name"
+                margin="dense"
+                variant="outlined"
+              />
+              <TextField
+                id="password"
+                fullWidth
+                label="Password"
+                margin="dense"
+                type="password"
+                variant="outlined"
+              />
+              <TextField
+                id="gtihub"
+                fullWidth
+                margin="dense"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <GitHub />
+                    </InputAdornment>
+                  ),
+                }}
+                defaultValue="https://github.com/"
+                variant="outlined"
+              />
+              <Box sx={{ py: 2 }}>
+                <Button
+                  color="primary"
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  style={{ background: "#FFFFFF", color: "black" }}
+                >
+                  Register
+                </Button>
+              </Box>
+              <Typography color="textSecondary" variant="body1" align="center">
+                Have an account?{" "}
+                <Link component={RouterLink} to="/login" variant="body1">
+                  Sign in
+                </Link>
+              </Typography>
+            </form>
           </Formik>
         </Container>
       </Box>

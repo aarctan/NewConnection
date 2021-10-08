d5923aa866ea178774e875140043309f86b9b83b
Exanut <nihilch@protonmail.com>
2021-10-08 16:26:06 -0600

    source handler code

--- server/newconnectionwhodis/signals/handlers.py
+++ server/newconnectionwhodis/signals/handlers.py
@@ -2,8 +2,9 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+# https://stackoverflow.com/a/59669317
 @receiver(post_save, sender=User)
-def default_to_non_active(sender, instance, created, **kwargs):
+def new_user_created(sender, instance, created, **kwargs):
     if created:
         print("user created")
         print(instance)

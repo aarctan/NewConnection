4f42c0302a312cfcbacb16495d2f67c098ffd218
Exanut <nihilch@protonmail.com>
2021-10-08 20:03:43 -0600

    create Author whenever a User is created

--- server/newconnectionwhodis/signals/handlers.py
+++ server/newconnectionwhodis/signals/handlers.py
@@ -1,10 +1,10 @@
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from .. import models
 
 # https://stackoverflow.com/a/59669317
 @receiver(post_save, sender=User)
 def new_user_created(sender, instance, created, **kwargs):
     if created:
-        print("user created")
-        print(instance)
+        models.Author.objects.create(user=instance, displayName=instance.username)

6342a1108eddfcdfb57ffb81ceb6908a4521b58a
Dylan Decoursey <69830467+dylandeco@users.noreply.github.com>
2021-10-12 19:51:15 -0600

    Added like button to comments, made text wrap around, may have to fix (#51)

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,6 +1,6 @@
 import { Box, Typography, Link } from "@mui/material";
-//import IconButton from "@mui/material/IconButton";
-//import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import IconButton from "@mui/material/IconButton";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 const Comment = (props) => {
   return (
@@ -8,23 +8,38 @@ const Comment = (props) => {
       sx={{
         display: "flex",
         flexDirection: "row",
-        justifyContent: "flex-start",
+        justifyContent: "space-between",
         alignItems: "center",
         backgroundColor: "#fafafa",
         py: 0.2,
       }}
     >
-      <Link
-        component="button"
-        variant="body2"
-        color="text.primary"
-        fontWeight="600"
-        underline="hover"
-        sx={{ marginRight: 1 }}
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          backgroundColor: "#fafafa",
+          py: 0.2,
+        }}
       >
-        {props.user}
-      </Link>
-      <Typography variant="body2">{props.comment}</Typography>
+        <Link
+          component="button"
+          variant="body2"
+          color="text.primary"
+          fontWeight="600"
+          underline="hover"
+          sx={{ marginRight: 1 }}
+        >
+          {props.user}
+        </Link>
+        <Typography variant="body2">{props.comment}</Typography>
+      </Box>
+      <IconButton aria-label="settings" sx={{ padding: 0.5 }}>
+        <FavoriteBorderIcon sx={{ width: 20, height: 20 }} />
+      </IconButton>
     </Box>
   );
 };

5e47b0fac13ffcf03ecc34142994c1ff20114693
Exanut <nihilch@protonmail.com>
2021-10-16 13:59:05 -0600

    cleanup login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,6 @@
+import { useState } from "react";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import * as Yup from "yup";
-import { Formik } from "formik";
 import { Box, Button, Link, TextField, Typography } from "@mui/material";
 import { useContext } from "react";
 import AuthContext from "src/store/auth-context";
@@ -19,6 +18,44 @@ const Login = () => {
   const navigate = useNavigate();
   const authCtx = useContext(AuthContext);
   const classes = useStyles();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    const url = `${API_URL}/dj-rest-auth/login/`;
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed!";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        authCtx.login(data.key);
+        navigate("/app/dashboard", { replace: true });
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
     <>
       <Helmet>
@@ -53,106 +90,54 @@ const Login = () => {
           borderRadius="10px"
           justifyContent="center"
         >
-          <Formik
-            initialValues={{
-              username: "",
-              password: "",
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
-            validationSchema={Yup.object().shape({
-              username: Yup.string().max(255).required("Username is required"),
-              password: Yup.string().max(255).required("Password is required"),
-            })}
-            onSubmit={(values) => {
-              const url = `${API_URL}/dj-rest-auth/login/`;
-              fetch(url, {
-                method: "POST",
-                body: JSON.stringify({
-                  username: values.username,
-                  password: values.password,
-                }),
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              })
-                .then((res) => {
-                  if (res.ok) {
-                    return res.json();
-                  } else {
-                    return res.json().then((data) => {
-                      let errorMessage = "Authentication failed!";
-                      // if (data && data.error && data.error.message) {
-                      //   errorMessage = data.error.message;
-                      // }
-
-                      throw new Error(errorMessage);
-                    });
-                  }
-                })
-                .then((data) => {
-                  authCtx.login(data.key);
-                  navigate("/app/dashboard", { replace: true });
-                })
-                .catch((err) => {
-                  alert(err.message);
-                });
+          />
+          <TextField
+            fullWidth
+            label="Username"
+            margin="dense"
+            name="username"
+            type="username"
+            variant="outlined"
+            onChange={(e) => {
+              setUsername(e.target.value);
             }}
-          >
-            {({ handleBlur, handleChange, handleSubmit, values }) => (
-              <form onSubmit={handleSubmit}>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                ></Box>
-                <TextField
-                  fullWidth
-                  label="Username"
-                  margin="dense"
-                  name="username"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="username"
-                  value={values.username}
-                  variant="outlined"
-                />
-                <TextField
-                  fullWidth
-                  label="Password"
-                  margin="dense"
-                  name="password"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="password"
-                  value={values.password}
-                  variant="outlined"
-                />
-                <Box sx={{ py: 2 }}>
-                  <Button
-                    color="primary"
-                    fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                    style={{ background: "#FFFFFF", color: "black" }}
-                  >
-                    Sign in
-                  </Button>
-                </Box>
-                <Typography
-                  color="textSecondary"
-                  variant="body1"
-                  align="center"
-                >
-                  Don&apos;t have an account?{" "}
-                  <Link component={RouterLink} to="/register" variant="body1">
-                    Sign up
-                  </Link>
-                </Typography>
-              </form>
-            )}
-          </Formik>
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            margin="dense"
+            name="password"
+            type="password"
+            variant="outlined"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <Box sx={{ py: 2 }}>
+            <Button
+              color="primary"
+              fullWidth
+              size="large"
+              type="submit"
+              variant="contained"
+              style={{ background: "#FFFFFF", color: "black" }}
+              onClick={handleLogin}
+            >
+              Sign in
+            </Button>
+          </Box>
+          <Typography color="textSecondary" variant="body1" align="center">
+            Don't have an account?{" "}
+            <Link component={RouterLink} to="/register" variant="body1">
+              Sign up
+            </Link>
+          </Typography>
         </Box>
       </Box>
     </>

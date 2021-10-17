b46e6ed781d092cbb2e7f2ac65482347c1e8a6cb
Exanut <nihilch@protonmail.com>
2021-10-17 00:03:00 -0600

    get posts from backend and map to Post components

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Box, Grid } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, Grid, Typography } from "@mui/material";
 import SideProfile from "./SideProfile";
 import CreatePostModal from "./CreatePostModal";
 import Post from "./Post";
@@ -10,11 +10,39 @@ const Feed = (props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    for (let i = 0; i < props.recentAuthors.length; i++) {
+      fetch(`${props.recentAuthors[i].id}/posts/`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          for (let j = 0; j < data.length; j++) {
+            setPosts((oldArray) => [...oldArray, data[j]]);
+          }
+        })
+        .catch((error) => console.log("Feed useEffect", error));
+    }
+  }, [props.recentAuthors]);
+
   return (
     <Box display="flex" mx="20%" my="85px">
       <Grid container spacing={4} justifyContent="flex-start">
         <Grid item xs={8}>
           <CreateNewPostContainer setIsModalOpen={setIsModalOpen} />
+          {posts.map((post, idx) => (
+            <Post
+              key={idx}
+              title={post.title}
+              description={post.description}
+              displayName={post.author.displayName}
+              contentType={post.contentType}
+              content={post.content}
+              count={idx}
+              profileImage="/static/images/avatars/rebecca.jpg"
+              comments={[]}
+            />
+          ))}
           {mockPosts.map((post, idx) => (
             <Post
               key={idx}

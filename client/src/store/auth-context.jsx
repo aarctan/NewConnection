19336eaa36ff86e0f7fccc8fde1269ee746755cd
Dylan Decoursey <69830467+dylandeco@users.noreply.github.com>
2021-10-17 18:13:46 -0600

    Userdata local storage now works in login (#54)

--- client/src/store/auth-context.jsx
+++ client/src/store/auth-context.jsx
@@ -4,7 +4,7 @@ const AuthContext = React.createContext({
   token: "",
   userdata: {},
   isLoggedIn: false,
-  login: (token) => {},
+  login: (token, userdata) => {},
   logout: () => {},
 });
 
@@ -16,34 +16,49 @@ const retrieveStoredToken = () => {
   };
 };
 
+const retrieveStoredUserData = () => {
+  const storedUserData = localStorage.getItem("userdata");
+
+  return {
+    userdata: storedUserData,
+  };
+};
+
 export const AuthContextProvider = (props) => {
   const tokenData = retrieveStoredToken();
-
+  const userData = retrieveStoredUserData();
   let initialToken;
+  let initialUserdata;
   if (tokenData) {
     initialToken = tokenData.token;
   }
-
+  if (userData) {
+    // Turn userdata into an object
+    initialUserdata = JSON.parse(userData.userdata);
+  }
   const [token, setToken] = useState(initialToken);
-  const [userdata, setUserdata] = useState({});
+  const [user, setUser] = useState(initialUserdata);
 
   const userIsLoggedIn = !!token;
 
   const logoutHandler = useCallback(() => {
     setToken(null);
-    setUserdata(null);
+    setUser(null);
     localStorage.removeItem("token");
+    localStorage.removeItem("userdata");
   }, []);
 
-  const loginHandler = (token, userdata) => {
+  const loginHandler = (token, user) => {
     setToken(token);
-    setUserdata(userdata);
+    setUser(user);
     localStorage.setItem("token", token);
+    // Need to turn JSON into a string to store in local storage
+    localStorage.setItem("userdata", JSON.stringify(user));
   };
 
   const contextValue = {
     token: token,
-    userdata: userdata,
+    userdata: user,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
     logout: logoutHandler,

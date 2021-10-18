e5de85c61580235ed0a5803caf9f07b7096ef78f
Dylan DeCoursey <decourse@ualberta.ca>
2021-10-17 20:23:38 -0600

    Drop down added to icon and logout handler added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,23 @@
-import * as React from "react";
-import { AppBar, Box, Toolbar, IconButton, Badge } from "@mui/material";
-import AccountCircle from "@mui/icons-material/AccountCircle";
+import { useContext, useState } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Badge,
+  Avatar,
+  Stack,
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import MailIcon from "@mui/icons-material/Mail";
+import Logout from "@mui/icons-material/Logout";
 import { makeStyles } from "@mui/styles";
 import Search from "src/components/Search";
+import AuthContext from "src/store/auth-context";
 
 const useStyles = makeStyles({
   logo: {
@@ -12,8 +26,23 @@ const useStyles = makeStyles({
 });
 
 const Header = () => {
-  const menuId = "primary-search-account-menu";
   const classes = useStyles();
+  const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const onLogoutHandler = () => {
+    authCtx.logout();
+    navigate("/login", { replace: true });
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -35,28 +64,73 @@ const Header = () => {
             alt="logo"
             className={classes.logo}
           />
-          <Search/>
+          <Search />
           <Box>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
-            >
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
+            <Stack alignItems="center" direction="row" spacing={1}>
+              <IconButton
+                size="large"
+                aria-label="show 4 new mails"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <MailIcon />
+                </Badge>
+              </IconButton>
+              <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
+                <Avatar
+                  alt="Avatar"
+                  src={authCtx.userdata.profileImage}
+                  sx={{ width: 28, height: 28 }}
+                />
+              </IconButton>
+            </Stack>
           </Box>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                width: 150,
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem>
+              <Avatar /> Profile
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={onLogoutHandler}>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>

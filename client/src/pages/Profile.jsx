17938ab9154f3a037aba581cbe3e15c83faf7ba8
Dylan DeCoursey <decourse@ualberta.ca>
2021-10-20 15:56:48 -0600

    Posts on profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import Header from "src/components/Header";
 import { useParams } from "react-router-dom";
 import Banner from "src/components/profile/Banner";
+import ProfileFeed from "src/components/profile/ProfileFeed";
 
 const Profile = () => {
   let { authorID } = useParams();
@@ -9,6 +10,7 @@ const Profile = () => {
     <>
       <Header />
       <Banner authorID={authorID} />
+      <ProfileFeed authorID={authorID} />
     </>
   );
 };

2feccf0902afc167ae9b75e536c2f051db44d6b9
Dylan Decoursey <69830467+dylandeco@users.noreply.github.com>
2021-10-31 13:32:00 -0600

    Styled likes modal (#87)

--- client/src/components/post/LikesModal.jsx
+++ client/src/components/post/LikesModal.jsx
@@ -1,26 +1,34 @@
-import { Avatar, Modal, Box, Typography, Stack, Link } from "@mui/material";
+import {
+  Avatar,
+  Modal,
+  MenuList,
+  Typography,
+  Box,
+  Divider,
+  MenuItem,
+  Link,
+} from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
 const style = {
   display: "flex",
   flexDirection: "column",
-  justifyContent: "space-around",
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   bgcolor: "background.paper",
   boxShadow: 20,
-  p: 3,
   borderRadius: "8px",
 };
 
-export default function LikesModal({ isModalOpen, setIsModalOpen, likes }) {
+const LikesModal = ({ isModalOpen, setIsModalOpen, likes }) => {
   const handleClose = () => setIsModalOpen(false);
   const theme = useTheme();
   const small = useMediaQuery(theme.breakpoints.down("sm"));
-  if (!isModalOpen) return null;
+  const navigate = useNavigate();
 
   return (
     <>
@@ -30,7 +38,7 @@ export default function LikesModal({ isModalOpen, setIsModalOpen, likes }) {
           style={
             small
               ? { width: "90%", height: "70%" }
-              : { width: 300, height: 400 }
+              : { width: 350, height: 350 }
           }
         >
           <Typography
@@ -38,26 +46,79 @@ export default function LikesModal({ isModalOpen, setIsModalOpen, likes }) {
             variant="h6"
             component="h2"
             align="center"
+            sx={{ padding: 1 }}
           >
-            Authors who liked this
+            Likes
           </Typography>
-
-          <Box display="flex" flexDirection="column">
-            {likes.map((like, idx) => (
-              <Stack alignItems="center" direction="row" spacing={1} key={idx}>
-                <Avatar
-                  alt="Avatar"
-                  src={like.author.profileImage}
-                  sx={{ width: 38, height: 38, border: 1, borderColor: "gray" }}
-                />
-                <Link component="button" variant="h6" underline="hover">
-                  {like.author.displayName}
-                </Link>
-              </Stack>
-            ))}
-          </Box>
+          <Divider />
+          <MenuList display="flex" flexDirection="column">
+            {likes.map((like, idx) => {
+              return (
+                <MenuItem key={idx} sx={{ height: 55 }}>
+                  <Box
+                    sx={{
+                      flexGrow: 1,
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      py: 0,
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Avatar
+                        alt="Avatar"
+                        src={like.author.profileImage}
+                        sx={{
+                          width: 38,
+                          height: 38,
+                          marginRight: 1,
+                          cursor: "pointer",
+                          border: 1,
+                          borderColor: "gray",
+                        }}
+                        onClick={() => {
+                          const words = like.author.id.split("/");
+                          const word = words[words.length - 1];
+                          navigate(`/app/author/${word}`, {
+                            state: like.author,
+                          });
+                        }}
+                      />
+                      <Link
+                        component="button"
+                        variant="body2"
+                        underline="hover"
+                        fontWeight="600"
+                        sx={{ marginRight: 1 }}
+                        onClick={() => {
+                          const words = like.author.id.split("/");
+                          const word = words[words.length - 1];
+                          navigate(`/app/author/${word}`, {
+                            state: like.author,
+                          });
+                        }}
+                      >
+                        {like.author.displayName}
+                      </Link>
+                    </Box>
+                  </Box>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
         </Box>
       </Modal>
     </>
   );
-}
+};
+
+export default LikesModal;
